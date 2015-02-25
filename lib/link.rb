f057189299a5d094b9079d519bb35cc32375c751
tommasobratto <tbratto22@gmail.com>
2015-02-25 11:35:58 +0000

    added Tag class

--- lib/link.rb
+++ lib/link.rb
@@ -6,4 +6,5 @@ class Link
   property :title,    String
   property :url,      String
 
+  has n, :tags, :through => Resource
 end
--- lib/server.rb
+++ lib/server.rb
@@ -6,6 +6,7 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
 
 DataMapper.finalize
 
@@ -15,3 +16,14 @@ get "/" do
   @links = Link.all 
   erb :index
 end
+
+post "/links" do 
+  url = params["url"]
+  title = params["title"]
+  tags = params["tags"].split(" ").map do |tag|
+    Tag.first_or_create(:text => tag)
+  end
+  Link.create(:url => url, :title => title, :tags => tags)
+  redirect to('/')
+end
+
