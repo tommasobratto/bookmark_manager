22a2fb064932fe8a45364de30fe4cee34637cb03
tommasobratto <tbratto22@gmail.com>
2015-02-25 13:54:01 +0000

    added sign up form

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'capybara'
 gem 'sinatra'
+gem 'bcrypt-ruby'
 
 group :test do 
   gem 'rspec'
--- lib/server.rb
+++ lib/server.rb
@@ -7,10 +7,15 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag'
+require './lib/user'
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
+enable :sessions
+
+set :session_secret, 'super secret'
 
 get "/" do 
   @links = Link.all 
@@ -33,3 +38,20 @@ get "/tags/:text" do
   erb :index 
 end
   
+get "/users/new" do
+  erb :"/users/new"
+end
+
+post "/users" do
+  user = User.create(:email => params[:email],
+                     :password => params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
+end
+
+helpers do
+
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
+end
